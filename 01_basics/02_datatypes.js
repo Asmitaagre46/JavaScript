728b21fa21ada75944f3e871615347370630487c
Asmita Agre <agreasmita46@gmail.com>
2024-09-18 10:13:18 +0000

    datatypes and ECMA script

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"     //treat all JS code as newer version
+
+//alert(3 + 3)  //we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Asmita")
+
+let name= "asmita"
+let age = 20
+let isLoggedIn = false
+let state;
+
+
+//premitive datatypes:
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+//objects
+
+console.log(typeof "asmita");
+console.log(typeof age);
+console.log(typeof null);    //object
+console.log(typeof undefined);   //undefined
+
