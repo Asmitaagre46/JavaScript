213dff9942ae24996b7c474d701c82fd9c7a4318
Asmita Agre <agreasmita46@gmail.com>
2024-09-19 07:33:29 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,31 @@
+const name = "Asmita"
+const age = 20
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
+// string interpolation concept     benefit: on the go we can add method to the variable
+
+const gameName = new String('asmita-aa')  //string declaration
+
+console.log(gameName[0]);   //key-value pair access (h)
+// methods
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 3)
+console.log(newString);
+
+const anotherString = gameName.slice(-6, 1)     //negative can be used here only.
+console.log(anotherString);
+
+const newName = "         asmita       "
+console.log(newName);
+console.log(newName.trim());   //for removing the extra spacing in text.
+
+const url = "https://asmita.com/asmita%20agre"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('asmita'));   //true
+
+console.log(gameName.split('-'));   // strings in the form of array.
