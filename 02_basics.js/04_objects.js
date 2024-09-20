7ee3aa14602cd2b7bdca2fc0b9468e56fd97e7e1
Asmita Agre <agreasmita46@gmail.com>
2024-09-20 18:44:15 +0000

    objects

---
+++ 02_basics.js/04_objects.js
@@ -0,0 +1,18 @@
+// object declaration by using constructor (singleton):
+const tinderUser = new Object()                  // singleton object delaration
+// const tinderUser = {}                            // literal object declaration
+
+tinderUser.id = "pqr123"
+tinderUser.name = "seema"
+tinderUser.isLoggedIn = false
+
+console.log(tinderUser);
+
+const regularUser = function{
+    email: "some@gmail.com",
+    fullname: {
+         userfullname: {
+            
+         }
+    }
+}
